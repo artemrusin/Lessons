0300a13f80dda764d60f9d6f4084b993b5a4e1d9
artemrusin <rusin2509@ro.ru>
2022-11-13 13:22:57 +0300

    UPD:
    Now this is function

--- Lesson_2/poisk.py
+++ Lesson_2/poisk.py
@@ -2,23 +2,32 @@ import math
 import random
 import numpy as np
 
-start=0
 
 
-lst=sorted(list(set(np.random.randint(1, 1000, random.randint(10,100)))))
 
-end=len(lst)-1
+'''lst=sorted(list(set(np.random.randint(1, 1000, random.randint(10,100)))))'''
 
-print(lst)
+def poisk_bin(start,finish,long,x):
+    start-=1
+    lst=sorted(list(set(np.random.randint(start, finish, long))))
+    end=long-1
+    while start<=end:
+        n=(start+end)//2
+        if lst[n]==x:
+            print(f'Find: {n} {lst[n]}')
+            break
+        elif lst[n]<x:
+            start=n+1
+        else:
+            end=n-1
+    print('Элемента нет')
 
-x=int(input())  
 
-while start<=end:
-    n=(start+end)//2
-    if lst[n]==x:
-        print(f'Find: {n} {lst[n]}')
-        break
-    elif lst[n]<x:
-        start=n+1
-    else:
-        end=n-1
+x=int(input())
+y=int(input())
+z=int(input())
+w=int(input())
+
+poisk_bin(x,y,z,w)
+
+
