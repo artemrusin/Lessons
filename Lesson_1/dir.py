9faffc19fb10d85a8bd89cad3821d812acee9ade
artemrusin <rusin2509@ro.ru>
2022-11-20 15:19:01 +0300

    UPD:New functions

--- Lesson_1/dir.py
+++ Lesson_1/dir.py
@@ -1,18 +1,40 @@
 import os
 
-def print_dir(path,lvl):
-    print('\t'*(lvl-1)+'├'+os.path.split(path)[-1])
-    print('\t'*(lvl-1)+'└'+'─'*7+'┐')
+def print_dir(path,lvl,print_all_info=False,print_part_info=False):
     files=os.listdir(path)
-    for file in files:
-        if os.path.isdir(path+"\\"+file):
-            print_dir(path+"\\"+file,lvl+1)
-        else:
-            print('\t'*lvl+'├'+file)
-    print('\t'*(lvl-1)+'┌'+'─'*7+'┘')
+    print('\t'*(lvl-1)+'├'+os.path.split(path)[-1])
+    if print_part_info:
+        x=0
+        y=0 
+        for file in files:
+            if os.path.isdir(path+"\\"+file):
+                y+=1
+            else:
+                x+=1
+        print('\t'*(lvl-1)+'│'+f'Файлов: {x},\n'+'\t'*(lvl-1)+f'│Папок: {y}')
+        y1=y
 
+    if print_part_info and y1==0:
+        pass
+    else:
+        print('\t'*(lvl-1)+'└'+'─'*7+'┐')
+        x=0
+        y=0
+        for file in files:
+            if os.path.isdir(path+"\\"+file):
+                files,dirs=print_dir(path+"\\"+file,lvl+1,print_part_info=print_part_info)
+                x+=files
+                y+=dirs+1
+            else:
+                print('\t'*lvl+'├'+file) if not print_part_info else 1
+                x+=1
+        print('\t'*(lvl-1)+'┌'+'─'*7+'┘')
+    if lvl==1 and print_all_info:
+        print(f'Файлов: {x},\nПапок: {y+1}') 
+    else:
+        return x,y
 
-print_dir(r'C:\Users\Nitro5\Downloads',1)
+print_dir(r'C:\Users\Nitro5\Desktop\Новая папка (2)',1)
 
 
 #│ └ ─ ├ ┘ ┰
